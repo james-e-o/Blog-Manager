9b33c79642760301c83750231e7af0d69e0108e4
jamiecruise <onwuasoanyajames@gmail.com>
2023-07-07 14:34:55 +0100

    office push07/7

--- src/App.js
+++ src/App.js
@@ -4,8 +4,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       jamie land
+       <h1>Skript<span>__n</span></h1>
+       <nav>
+       <svg data-name="Layer 1" id="Layer_1" viewBox="0 0 64 64" width="30px" height="30px" className="cursor-pointer fill-gray-200 pt-1" xmlns="http://www.w3.org/2000/svg"><path d="M21.86,18.73H9.18a2,2,0,0,1,0-4H21.86a2,2,0,0,1,0,4Z"/><path d="M54.82,18.73H34.88a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/><path d="M54.82,34H9.18a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/><path d="M54.82,49.27H30.07a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/></svg>
+        <ul>
+          <li>Sign up</li>
+          <li>Login</li>
+        </ul>
+       </nav>
       </header>
+      <main>
+        <div className='main'>
+          <div className='h1-tag'>
+            <h1>Create <span>Manage and Publish </span>engaging blog contents <span>with our cms</span>.</h1>
+          </div>
+          <div className='section'>
+            <p>
+            Veniam pariatur cillum ullamco proident et id Lorem excepteur ad. Commodo qui excepteur tempor veniam deserunt eu dolore eu.<br />
+            <button>Start now</button>
+            </p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
