b58e606984a469967bb9463b0063a73578d39900
jamiecruise <onwuasoanyajames@gmail.com>
2023-08-10 18:52:45 +0100

    office push10/8(2)blogCms

--- src/App.js
+++ src/App.js
@@ -11,19 +11,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Skript<span>__n</span></h1>
+       <h1>Skript_<span>n</span></h1>
        <nav>
-       <svg data-name="Layer 1" id="Layer_1" viewBox="0 0 64 64" width="30px" height="30px" className="cursor-pointer fill-gray-200 pt-1" xmlns="http://www.w3.org/2000/svg"><path d="M21.86,18.73H9.18a2,2,0,0,1,0-4H21.86a2,2,0,0,1,0,4Z"/><path d="M54.82,18.73H34.88a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/><path d="M54.82,34H9.18a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/><path d="M54.82,49.27H30.07a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/></svg>
-        <ul>
+       <li>&#9998; Write</li>
+       <svg data-name="Layer 1" id="Layer_1" viewBox="0 0 64 64" width="30px" height="30px" xmlns="http://www.w3.org/2000/svg"><path d="M21.86,18.73H9.18a2,2,0,0,1,0-4H21.86a2,2,0,0,1,0,4Z"/><path d="M54.82,18.73H34.88a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/><path d="M54.82,34H9.18a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/><path d="M54.82,49.27H30.07a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/></svg>
+       <ul>
           <li>Sign up</li>
           <li>Login</li>
-        </ul>
+       </ul>
        </nav>
       </header>
       <main>
         <div className='main'>
           <div className='h1-tag'>
-            <div className='opaque'></div>
             <h1>Write<span>, Manage & Publish </span>engaging articles <span>with us</span>.</h1>
             <p id='cta'>
             Veniam pariatur cillum ullamco proident et id Lorem excepteur ad. Commodo qui excepteur tempor veniam deserunt eu dolore eu.<br />
