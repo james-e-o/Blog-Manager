993f70276b4fc7156246eea0bca24145a1b62317
jamiecruise <onwuasoanyajames@gmail.com>
2023-08-11 09:59:23 +0100

    office push11/8(2)blogCms

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
        <h1>Skript_<span>n</span></h1>
        <nav>
-       <li>&#9998; Write</li>
+       <button>&#9998; Write</button>
        <svg data-name="Layer 1" id="Layer_1" viewBox="0 0 64 64" width="30px" height="30px" xmlns="http://www.w3.org/2000/svg"><path d="M21.86,18.73H9.18a2,2,0,0,1,0-4H21.86a2,2,0,0,1,0,4Z"/><path d="M54.82,18.73H34.88a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/><path d="M54.82,34H9.18a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/><path d="M54.82,49.27H30.07a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/></svg>
        <ul>
           <li>Sign up</li>
