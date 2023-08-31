16a1fe54595edff2c9b3e9fae75959dab89e9b88
jamiecruise <onwuasoanyajames@gmail.com>
2023-08-31 18:50:34 +0100

    office push31(2)blogCms

--- src/sign.js
+++ src/sign.js
@@ -29,7 +29,26 @@ export const Sign = () => {
 
 export const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='signup'>
+      <h4 style={{padding:'5px 15px'}}>Sign up</h4>
+      <div className="form">
+       <p>Username:</p>
+        <input type="email" />
+      </div>
+      <div className="form">
+       <p>Email:</p>
+        <input type="email" />
+      </div>
+      <div className="form">
+       <p>Password:</p>
+        <input type="email" />
+      </div>
+      <div className="form">
+       <p>Re-enter Password:</p>
+        <input type="email" />
+      </div>
+      <button style={{padding: '8px 70px',fontSize:'15px',color:'white', background:'slateblue', border:'none', borderRadius:'4px',marginTop:'15px'}}>Sign up</button>
+    </div>
   )
 }
 export const Signin = () => {
@@ -51,8 +70,8 @@ export const Signin = () => {
             <button style={{padding: '8px 28px',fontSize:'15px',color:'white', background:'slateblue', border:'none', borderRadius:'4px',}}>Slide in</button>
           </div>
           <div style={{display:'flex',justifyContent:'space-between', marginTop:'15px', fontSize:'14px',color:'slategray'}}><hr style={{width:'38%',  color:'#d1cfe2',position:'relative',top:'9px'}}/> or <hr style={{width:'38%', color:'#d1cfe2', position:'relative',top:'9px'}}/></div>
-          <p style={{display:'flex', fontSize:'15px',justifyContent:'center', gap:'8px',marginTop:'15px',border:'1px solid grey',borderRadius:'30px', padding:'5px'}}><Google idth='48' eight='48'/> Continue with Google</p>
-          <div style={{fontSize:'15px', marginTop:'10px',padding:'5px', color:'#6559ac'}}>Create Account</div>
+          <p style={{display:'flex', fontSize:'14px',fontWeight:'light',justifyContent:'center', gap:'8px',marginTop:'15px',border:'1px solid grey',borderRadius:'30px', padding:'5px'}}><Google idth='48' eight='48'/> Continue with Google</p>
+          <div style={{fontSize:'14px',fontWeight:'light', marginTop:'10px',padding:'5px', color:'#6559ac'}}>Create Account</div>
 
       </div>
 </div>
