a789e528c99c42aeb9d45af586723c26303a8deb
jamiecruise <onwuasoanyajames@gmail.com>
2023-09-16 16:26:29 +0100

    office push09/16(2)blogCms

--- src/Components/dashboards/UserDashboard.js
+++ src/Components/dashboards/UserDashboard.js
@@ -16,7 +16,7 @@ const UserDashboard = () => {
         <>
           <nav >
             <button >&#9998; </button>  
-            <figure>hol</figure>
+            <figure></figure>
             <div className='svg'>          
                   <svg  data-name="Layer 1" id="Layer_1" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg"><path d="M21.86,18.73H9.18a2,2,0,0,1,0-4H21.86a2,2,0,0,1,0,4Z"/><path d="M54.82,18.73H34.88a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/><path d="M54.82,34H9.18a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/><path d="M54.82,49.27H30.07a2,2,0,0,1,0-4H54.82a2,2,0,0,1,0,4Z"/></svg>
             </div>        
