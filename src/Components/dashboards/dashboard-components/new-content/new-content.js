56167541b7ed440823cbb4e63b86108cc8db6f3a
Onwuasoanya James <onwuasoanyajames@gmail.com>
2024-04-11 19:57:10 +0100

    blog4/11 home(1)

--- src/Components/dashboards/dashboard-components/new-content/new-content.js
+++ src/Components/dashboards/dashboard-components/new-content/new-content.js
@@ -2,16 +2,18 @@ import React, { useEffect, useState } from 'react'
 import "./new-content.css"
 import "./menu-drop.css"
 import {Menu_icon, Plus } from '../../../svg'
-import alignLeft from "../../../contents/images/align-left.png"
-import alignCentre from "../../../contents/images/align-center.png"
-import alignRight from "../../../contents/images/align-right.png"
-import alignJustify from "../../../contents/images/align-justify.png"
-import Indent from "../../../contents/images/indent.png"
-import Outdent from "../../../contents/images/outdent.png"
-import Paragraph from "../../../contents/images/paragraph.png"
-import Image from "../../../contents/images/image-60.png"
-import Undo from "../../../contents/images/undo-48.png"
-import Redo from "../../../contents/images/redo-48.png"
+import Paragraph from "../../../contents/images/icons/para.png"
+import Image from "../../../contents/images/icons/images.png"
+import Undo from "../../../contents/images/icons/undo.png"
+import Redo from "../../../contents/images/icons/redo.png"
+import Bullet from "../../../contents/images/icons/bullet.png"
+import Numbered from "../../../contents/images/icons/number.png"
+import Code from "../../../contents/images/icons/code.png"
+import Strike from "../../../contents/images/icons/strike.png"
+import Bold from "../../../contents/images/icons/bold.png"
+import Italics from "../../../contents/images/icons/italic.png"
+import Under from "../../../contents/images/icons/underline.png"
+import Line from "../../../contents/images/icons/line.png"
 
 const NewContent = () => {
   const [activeMenu, setActiveMenu] = useState(false) 
@@ -97,22 +99,56 @@ export default NewContent
 
 import { EditorProvider, FloatingMenu, BubbleMenu, useCurrentEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import Underline from '@tiptap/extension-underline'
 import ListItem from '@tiptap/extension-list-item'
 
 const extensions = [
-  StarterKit,
+  StarterKit, Underline
 ]
 
 const content = '<'
 const EditorMenu = () => {
   const { editor } = useCurrentEditor()
+  const controlsScroll = document.getElementById("controls-scroll")
+  const menuLeftArr = document.getElementById("leftarr")
+  const menuRightArr = document.getElementById("rightarr")
 
   if (!editor) {
     return null
   }
 
   return (
-    <div className='editor-controls'>
+    <div className='editor-controls' onClick={(e) => e.preventDefault()}>
+      <p id="leftarr" onClick={()=>{
+          controlsScroll.scrollTo({
+            top: 0,
+            left: (controlsScroll.scrollLeft - 45%(controlsScroll.scrollWidth - controlsScroll.offsetWidth)),
+            behavior: "smooth",
+          });
+      }} ></p>
+      <p id="rightarr" onClick={()=>{
+          controlsScroll.scrollTo({
+            top: 0,
+            left: (controlsScroll.scrollLeft + 45%(controlsScroll.scrollWidth - controlsScroll.offsetWidth)),
+            behavior: "smooth",
+          });
+      }}></p>
+      <div id="controls-scroll" className="controls-scroll" onScroll={(e)=>{
+          if (e.target.scrollLeft > 27){
+            menuLeftArr.style.zIndex = "1"
+            menuLeftArr.style.opacity="1"
+          } else {
+            menuLeftArr.style.zIndex = "-1"
+            menuLeftArr.style.opacity="0"
+          } 
+          if (e.target.scrollLeft >= (e.target.scrollWidth - (e.target.offsetWidth + 20))){
+            menuRightArr.style.zIndex = "-1"
+            menuRightArr.style.opacity="0"
+          } else {
+            menuRightArr.style.zIndex = "1"
+            menuRightArr.style.opacity="1"
+          }
+      }}>
       <button
         onClick={(e) => {e.preventDefault(), editor.chain().focus().toggleBold().run()}}
         disabled={
@@ -124,7 +160,7 @@ const EditorMenu = () => {
         }
         className={editor.isActive('bold') ? 'is-active' : ''}
       >
-        B
+        <img src={Bold} height={15} width={15}/>
       </button>
       <button
         onClick={(e) => {e.preventDefault(), editor.chain().focus().toggleItalic().run()}}
@@ -137,21 +173,18 @@ const EditorMenu = () => {
         }
         className={editor.isActive('italic') ? 'is-active' : ''}
       >
-        <i>I</i>
+        <img src={Italics} height={15} width={15}/>
       </button>
-      {/* <button
-        onClick={() => editor.chain().focus().toggleUnderline().run()}
+      <button
+        onClick={(e) => {e.preventDefault(), editor.commands.toggleUnderline()}}
         disabled={
-          !editor.can()
-            .chain()
-            .focus()
+          !editor.commands
             .toggleUnderline()
-            .run()
         }
         className={editor.isActive('underline') ? 'is-active' : ''}
       >
-        <i>U</i>
-      </button> */}
+       <img src={Under} height={15} width={15}/>
+      </button>
       <button
         onClick={(e) => {e.preventDefault(), editor.chain().focus().toggleStrike().run()}}
         disabled={
@@ -163,7 +196,7 @@ const EditorMenu = () => {
         }
         className={editor.isActive('strike') ? 'is-active' : ''}
       >
-        <strike>S</strike>
+        <img src={Strike} height={15} width={15}/>
       </button>
       <button
         onClick={(e) => {e.preventDefault(), editor.chain().focus().toggleCode().run()}}
@@ -176,85 +209,19 @@ const EditorMenu = () => {
         }
         className={editor.isActive('code') ? 'is-active' : ''}
       >
-        code
+        <img src={Code} height={19} width={19}/>
       </button>
-      <button onClick={(e) => {e.preventDefault(), editor.chain().focus().unsetAllMarks().run()}}>
+      {/* <button onClick={(e) => {e.preventDefault(), editor.chain().focus().unsetAllMarks().run()}}>
         clear marks
       </button>
       <button onClick={() => editor.chain().focus().clearNodes().run()}>
         clear nodes
-      </button>
+      </button> */}
       <button
         onClick={() => editor.chain().focus().setParagraph().run()}
         className={editor.isActive('paragraph') ? 'is-active' : ''}
       >
         <img src={Paragraph} height={15} width={15} alt="" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
-      >
-        h1
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
-      >
-        h2
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
-      >
-        h3
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
-      >
-        h4
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-        className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
-      >
-        h5
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-        className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
-      >
-        h6
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive('bulletList') ? 'is-active' : ''}
-      >
-        bullet list
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive('orderedList') ? 'is-active' : ''}
-      >
-        <img src={Paragraph} height={15} width={15} alt="" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive('codeBlock') ? 'is-active' : ''}
-      >
-        code block
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={editor.isActive('blockquote') ? 'is-active' : ''}
-      >
-        blockquote
-      </button>
-      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-        horizontal rule
-      </button>
-      <button onClick={() => editor.chain().focus().setHardBreak().run()}>
-        hard break
       </button>
       <button
         onClick={() => editor.chain().focus().undo().run()}
@@ -281,11 +248,64 @@ const EditorMenu = () => {
         <img src={Redo} height={15} width={15} alt="" />
       </button>
       <button
-        onClick={() => editor.chain().focus().setColor('#958DF1').run()}
-        className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
+        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
       >
-        purple
+       <h4 style={{color:"#9500BA"}}>H1</h4>
       </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+      >
+        <h4 style={{color:"#9500BA"}}>H2</h4>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
+      >
+        <h4 style={{color:"#9500BA"}}>H3</h4>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+        className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
+      >
+        <h4 style={{color:"#9500BA"}}>H4</h4>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+        className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
+      >
+        <h4 style={{color:"#9500BA"}}>H5</h4>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+        className={editor.isActive('bulletList') ? 'is-active' : ''}
+      >
+        <img src={Bullet} height={19} width={19}/>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        className={editor.isActive('orderedList') ? 'is-active' : ''}
+      >
+        <img src={Numbered} height={19} width={19} alt="" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+        className={editor.isActive('codeBlock') ? 'is-active' : ''}
+      >
+        code block
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBlockquote().run()}
+        className={editor.isActive('blockquote') ? 'is-active' : ''}
+      >
+        blockquote
+      </button>
+      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+      <img src={Line} height={20} width={20} />
+      </button>
+  
+      </div>
     </div>
   )
 }
@@ -295,8 +315,7 @@ const Editor = () => {
   return (
       <div className="editor">
         <EditorProvider slotBefore={<EditorMenu />} extensions={extensions} >
-          {/* <FloatingMenu>This is the floating menu</FloatingMenu>
-          <BubbleMenu>This is the bubble menu</BubbleMenu> */}
+          
         </EditorProvider>
       </div>  
   )
