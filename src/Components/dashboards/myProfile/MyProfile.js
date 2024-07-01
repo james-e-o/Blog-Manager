3a972d535554239bf0f28bc660484311123d5396
Onwuasoanya James <onwuasoanyajames@gmail.com>
2024-07-01 03:35:53 +0100

    blog7/1 master(1)

--- src/Components/dashboards/myProfile/MyProfile.js
+++ src/Components/dashboards/myProfile/MyProfile.js
@@ -1,30 +1,60 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import "./my-profile.css"
+import UserImg from "../feed/gemini-profile.jpeg"
+import { screenWidth } from '../../app/App'
 
 const MyProfile = () => {
     const [editProfile, setEditProfile] = useState(false)
+    const screen = useContext(screenWidth)
   return (
-    <>
-        {!editProfile?
-            <div>
-                <figure>
-                    <img src="" alt="" />
-                </figure>
-                <p className="my-name">Jason Statham</p>
-                <p className="edit-profile">Edit profile</p>
-            </div>
-            :
-            <div>
-                <figure>
-                    <img src="" alt="" />
-                </figure>
-                <p className="my-name">Jason Statham</p>
-                <p className="edit-profile">Edit profile</p>
-            </div>
-        }
+    <> 
+{
+        //  MOBILE
+        screen<480?(
+          <div className='m-my-profile'>
+             {!editProfile?
+                <div className='non-edit'>
+                    <div className="profile-image-wrap">
+                        <figure>
+                            <img src={UserImg} width={'100%'} height={'100%'} />
+                        </figure>
+                    </div>
+                    <p className="my-name">Jason Statham</p>
+                    <p className="profile-options">
+                        <button className="edit-profile">Edit profile</button>
+                        <button className="subscriptions">View following</button>
+                    </p>
+                </div>
+                :
+                <div>
+                    <figure>
+                        <img src="" alt="" />
+                    </figure>
+                    <p className="my-name">Jason Statham</p>
+                    <p className="edit-profile">Edit profile</p>
+                </div>
+            }
+            
+          </div>
+          )
+          // TABLET
+          :screen<720?(
+          <div className='t-my-profile'>
+            
+          </div>
+          ):
+          //DESKTOP 
+          (
+          <div className='d-my-profile'>
+           
+          </div>
+           
+          )}
+   
     </>
     
   )
 }
 
 export default MyProfile
+
